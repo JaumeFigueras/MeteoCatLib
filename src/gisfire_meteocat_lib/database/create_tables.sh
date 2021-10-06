a17c42715e3c0d12c927d9fe0e7839da327dae6a
JaumeFigueras <jaume.figueras@upc.edu>
2021-10-06 16:32:39 +0200

    Removed reference as it is an overhead without value. Is can be useful to MeteoCap but not to GisFIRE

--- src/gisfire_meteocat_lib/database/create_tables.sh
+++ src/gisfire_meteocat_lib/database/create_tables.sh
@@ -9,4 +9,3 @@ FOLDER=$2
 PORT=$3
 
 sudo -u postgres -- psql -p "$PORT" -d "$CLUSTER_NAME"db -f "$FOLDER"/meteocat_xema.sql
-sudo -u postgres -- psql -p "$PORT" -d "$CLUSTER_NAME"db -f "$FOLDER"/meteocat_ref.sql
--- test/conftest.py
+++ test/conftest.py
@@ -10,13 +10,11 @@ socket_dir = tempfile.TemporaryDirectory()
 postgresql_session = factories.postgresql_proc(port=None, unixsocketdir=socket_dir.name)
 postgresql_schema = factories.postgresql('postgresql_proc', dbname='test', load=[
     str(test_folder) + '/database_init.sql',
-    str(test_folder.parent) + '/src/gisfire_meteocat_lib/database/meteocat_ref.sql',
     str(test_folder.parent) + '/src/gisfire_meteocat_lib/database/meteocat_xdde.sql',
     str(test_folder.parent) + '/src/gisfire_meteocat_lib/database/meteocat_xema.sql'])
 
 pytest_plugins = [
    'test.fixtures.sqlalchemy',
    'test.fixtures.meteocat.meteocat',
-   'test.fixtures.meteocat.ref',
    'test.fixtures.meteocat.xema',
 ]
