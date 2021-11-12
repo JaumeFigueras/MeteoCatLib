44697ef5125c5785060789d2bbc35dc053195ba3
JaumeFigueras <jaume.figueras@upc.edu>
2021-11-12 14:08:01 +0100

    Added pad zeroes to URL

--- src/gisfire_meteocat_lib/remote_api/meteocat_urls.py
+++ src/gisfire_meteocat_lib/remote_api/meteocat_urls.py
@@ -11,4 +11,4 @@ STATION_AUXILIAR_VARIABLES = 'https://api.meteo.cat/xema/v1/estacions/{0:}/varia
 STATION_MEASURED_DATA = 'https://api.meteo.cat/xema/v1/variables/mesurades/{0:}/{1:}/{2:}/{3:}?codiEstacio={4:}'
 STATION_MULTI_DATA = 'https://api.meteo.cat/xema/v1/variables/cmv/{0:}/{1:}/{2:}/{3:}?codiEstacio={4:}'
 STATION_AUXILIAR_DATA = 'https://api.meteo.cat/xema/v1/variables/auxiliars/{0:}/{1:}/{2:}/{3:}?codiEstacio={4:}'
-LIGHTNINGS_DATA = 'https://api.meteo.cat/xdde/v1/catalunya/{0:}/{1:}/{2:}/{3:}'
+LIGHTNINGS_DATA = 'https://api.meteo.cat/xdde/v1/catalunya/{0:}/{1:02d}/{2:02d}/{3:02d}'
